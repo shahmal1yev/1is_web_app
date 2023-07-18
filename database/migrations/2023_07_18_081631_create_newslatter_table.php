dc18733aa0992f8cfc7ba0a9fd577100b4aa397e
Eldar Shahmaliyev <shahmal1yev@mail.ru>
2023-07-18 12:17:08 +0400

    newslatter uchun migration yaradildi. test edildi

---
+++ database/migrations/2023_07_18_081631_create_newslatter_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('newslatter', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('email')->default(null);
+            $table->boolean('status')->default(false);
+
+            $table->dateTime('created_at')->useCurrent();
+            $table->dateTime('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('newslatter');
+    }
+};
