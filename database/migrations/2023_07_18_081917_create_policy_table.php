0f8f4c57c3a9788424ad03aced613982578343ac
Eldar Shahmaliyev <shahmal1yev@mail.ru>
2023-07-18 12:19:39 +0400

    policy table uchun migration yaradildi. test edildi

---
+++ database/migrations/2023_07_18_081917_create_policy_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('policy', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->text('content_az');
+            $table->text('content_en');
+            $table->text('content_ru');
+            $table->text('content_tr');
+            $table->boolean('status')->default(true);
+
+
+            $table->dateTime('created_at')->useCurrent();
+            $table->dateTime('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('policy');
+    }
+};
