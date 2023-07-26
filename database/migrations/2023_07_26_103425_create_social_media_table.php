996e099f1d5ed1e3a36ce4843d10a06528b180d8
Eldar Shahmaliyev <shahmal1yev@mail.ru>
2023-07-26 14:34:49 +0400

    social media table uchun migration yaradildi test edildi

---
+++ database/migrations/2023_07_26_103425_create_social_media_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('social_media', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('facebook');
+            $table->string('instagram');
+            $table->string('linkedin');
+
+            $table->boolean('status')->default(true);
+
+            $table->dateTime('created_at')->useCurrent();
+            $table->dateTime('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('social_media');
+    }
+};
