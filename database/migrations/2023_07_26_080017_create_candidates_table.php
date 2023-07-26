98dd64824c3068bf45329b65112e2cc61efc0e2f
Eldar Shahmaliyev <shahmal1yev@mail.ru>
2023-07-26 12:11:23 +0400

    candidates table uchun migration yaradildi. test edildi

---
+++ database/migrations/2023_07_26_080017_create_candidates_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('candidates', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('name')->nullable();
+            $table->string('surname')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('mail')->nullable();
+            $table->string('cv')->nullable();
+
+            $table->bigInteger('vacancy_id')->nullable();
+            $table->bigInteger('user_id')->nullable();
+
+            $table->dateTime('created_at')->useCurrent();
+            $table->dateTime('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('candidates');
+    }
+};
