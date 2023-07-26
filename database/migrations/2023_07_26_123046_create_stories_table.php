c69c4f4d7397d1267abf3524060f528975dc9f61
Eldar Shahmaliyev <shahmal1yev@mail.ru>
2023-07-26 16:31:37 +0400

    stories table uchun migration yaradildi test edildi

---
+++ database/migrations/2023_07_26_123046_create_stories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stories', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->longtext('stories');
+
+            $table->string('redirect_link')->nullable();
+
+            $table->string('image');
+
+            $table->boolean('status')->default(true);
+
+            $table->dateTime('created_at')->useCurrent();
+            $table->dateTime('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stories');
+    }
+};
