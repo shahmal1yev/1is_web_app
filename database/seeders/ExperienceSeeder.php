7f5139c7dab00abca256f70e14f5c9d0f6e7ecc8
Eldar Shahmaliyev <shahmal1yev@mail.ru>
2023-07-30 15:28:17 +0400

    Experience seeder yazildi. Test edildi

---
+++ database/seeders/ExperienceSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Experiences;
+
+class ExperienceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $filePath = base_path() . "/database/JSON/experiences.json";
+    	$json_data = file_get_contents($filePath);
+    	$json = json_decode($json_data, true);
+    	$datas = $json[2]['data'];
+
+    	foreach($datas as $data)
+    	{
+    		Experiences::create($data);
+    	}
+    }
+}
