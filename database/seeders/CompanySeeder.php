91165e931be589931a5957cd56cd6917381dc857
Eldar Shahmaliyev <shahmal1yev@mail.ru>
2023-07-30 14:37:34 +0400

    Company seeder yazildi. Test edildi

---
+++ database/seeders/CompanySeeder.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Illuminate\Support\Str;
+
+use App\Models\Companies;
+
+
+class CompanySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $filePath = base_path() . "/database/JSON/companies.json";
+    	$json_data = file_get_contents($filePath);
+    	$json = json_decode($json_data, true);
+    	$datas = $json[2]['data'];
+
+
+
+    	foreach($datas as $data)
+    	{
+    		$instagram = ($data['instagram'] == "\"\"") ? null : $data['instagram'];
+    		$linkedin = ($data['linkedin'] == "\"\"") ? null : $data['linkedin'];
+    		$twitter = ($data['twitter'] == "\"\"") ? null : $data['twitter'];
+    		$facebook = ($data['facebook'] == "\"\"") ? null : $data['facebook'];
+
+    		$imagePath = $data['logo'];
+    		$imagePathPieces = explode('/', $imagePath);
+    		$imageName = end($imagePathPieces);
+    		$imageNewPath = "/back/assets/images/companies/" . $imageName;
+
+    		Companies::create([
+    			'user_id' => $data['user_id'],
+    			'sector_id' => $data['sector'],
+
+    			'name' => $data['title'],
+
+    			'slug' => Str::slug($data['title']),
+
+    			'about' => $data['about'],
+    			'hr' => $data['hr'],
+
+    			'address' => $data['address'],
+    			'website' => $data['website'],
+
+    			'map' => $data['map'],
+
+    			'instagram' => $instagram,
+    			'linkedin' => $linkedin,
+    			'facebook' => $facebook,
+    			'twitter' => $twitter,
+
+    			'image' => $imageNewPath,
+
+    			'view' => $data['view'],
+
+    			'status' => $data['status'],
+    		]);
+    	}
+    }
+}
