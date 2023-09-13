1ab052a2cf4043e88df5034833d5fade399567ba
server <empty>
2023-09-13 06:40:59 +0000

    server commit

--- app/Http/Controllers/Front/Contact/ContactFrontController.php
+++ app/Http/Controllers/Front/Contact/ContactFrontController.php
@@ -68,7 +68,7 @@ class ContactFrontController extends Controller
             return redirect()->route('contactindex')->with('success',  __('messages.murgonder'));
     
         } catch (\Throwable $e) {
-            return redirect()->route('contactindex')->with('error', __('messages.nesexeta'));
+            return redirect()->route('contactindex')->with('error', __("messages.nesexeta"));
         }
     }
 }
